15deb751772dc7703fa58a032328cc4786f13f0a
trist <tristanwilson111@gmail.com>
2017-08-23 00:31:24 -0400

    Removed title bar, moved code to start onButtonClick, changed timing words to button text after click and "Times Up" message to appear on button on finish. Changed background to pink. Resized FrameLayout to be positioned correctly.

--- app/src/main/java/com/icaninteractive/tristan/taskapplication/MainActivity.java
+++ app/src/main/java/com/icaninteractive/tristan/taskapplication/MainActivity.java
@@ -6,6 +6,8 @@ import android.net.Uri;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import com.timqi.sectorprogressview.SectorProgressView;
 
@@ -31,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
 
     /*
     ToDo: Align layout correctly
-    ToDo: Add Start button, move code to onClickListener(). Remove code form onCreate()
     ToDo: Add Task Completed, Task Not Completed buttons to onFinish()
     ToDo: Create a tracker to keep tasks recorded.
     ToDo: Decide whether the timer will be in intervals of 5, or to fix progress wheel not finishing if stopping on number not multiple of 5
@@ -44,35 +45,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView countdownText = (TextView) findViewById(R.id.countdownText);
-        //Set the spv object to a new SectorProgressView, linked to our spv id in activity_main.xml
-        spv = (SectorProgressView) findViewById(R.id.spv);
 
-        //Create Countdown Timer Object that runs for 45 seconds (45,000 Milliseconds)
-        //For some reason, the timer stops at 43. Adding 2000 milliseconds resolves this issue.
-        new CountDownTimer(((userNum*1000)+1000), 1000) {
-            public void onTick(long millisUntilFinished) {
-                muf = millisUntilFinished;
-                suf = muf/1000;
-                //Set top-left number to Milliseconds until finished / 1000, to show seconds until finished
-                countdownNum++;
-                spv.setPercent((float)progress);
-                progress = progress+timerPercent;
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
 
-                countdownText.setText(suf+"s");
+                //Set the spv object to a new SectorProgressView, linked to our spv id in activity_main.xml
+                spv = (SectorProgressView) findViewById(R.id.spv);
 
+                //Create Countdown Timer Object that runs for 45 seconds (45,000 Milliseconds)
+                //For some reason, the timer stops at 43. Adding 2000 milliseconds resolves this issue.
+                new CountDownTimer(((userNum*1000)+1000), 1000) {
+                    public void onTick(long millisUntilFinished) {
+                        muf = millisUntilFinished;
+                        suf = muf/1000;
+                        button.setText(suf+" seconds");
+                        //Set top-left number to Milliseconds until finished / 1000, to show seconds until finished
+                        countdownNum++;
+                        spv.setPercent((float)progress);
+                        progress = progress+timerPercent;
+
+                    }
+                    public void onFinish() {
+                        spv.setPercent((float)progress);
+                        button.setText("Time's up!");
+                        try {
+                            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                            Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+                            r.play();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
             }
-            public void onFinish() {
-                spv.setPercent((float)progress);
-                countdownText.setText("Time's up!");
-                try {
-                    Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-                    Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
-                    r.play();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
+        });
     }//End of onCreate Method
 }//End of MainActivity
